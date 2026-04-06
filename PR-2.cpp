b5ee20ee0b1dbdf3e14ef816baf5f8659e321a55
sg1687054-design <sg1687054@gmail.com>
2026-04-06 16:09:40 +0530

    Add files via upload

---
+++ PR-2.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+	int marks;
+	char grade;
+	
+	printf("enter your score (0-100):");
+	scanf("%d\n",marks);
+	 
+	grade = (marks >= 90) ?'A':
+		    (marks >= 80) ?'B':
+		    (marks >= 70) ?'C':
+			(marks >= 60) ?'D':
+			(marks >= 50) ?'F':
+			
+	printf("\nyour grade is %c" , grade);
+
+
+switch(grade) {
+	case 'A':
+		printf("Excellent Work");
+		break;
+	case 'B':
+		printf("Well Done");
+		break;
+	case 'C':
+		printf("Good Job");
+		break;
+	case 'D':
+		printf("You passed but you could do better");
+		break;
+	case 'F':
+		printf("You Failed");
+		break;	
+}
+
+if(grade >= 'A' && grade <= 'D'){
+		printf("You are eligible for next level.");
+}else{
+		printf("Please try again next exam.");
+    }
+return 0;
+}
